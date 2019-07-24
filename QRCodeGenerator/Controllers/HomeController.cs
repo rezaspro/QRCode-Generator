a0698c58dafcd3abc6d9759c29b3dd1236115d0a
Md. Shakil Reza <rezaspro@gmail.com>
2019-07-24 17:40:27 +0600

    Initial commit

---
+++ QRCodeGenerator/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QRCodeGenerator.Models;
+
+namespace QRCodeGenerator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            QRCoder.QRCodeGenerator qRCodeGenerator = new QRCoder.QRCodeGenerator();
+            QRCoder.QRCodeData qRCodeData = qRCodeGenerator.CreateQrCode("Shakil", QRCoder.QRCodeGenerator.ECCLevel.H);
+            QRCoder.QRCode qrCode = new QRCoder.QRCode(qRCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(12);
+
+            var bitmapBytes = BitmapToBytes(qrCodeImage); //Convert bitmap into a byte array
+            return File(bitmapBytes, "image/jpeg"); //Return as file result
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private static byte[] BitmapToBytes(Bitmap img)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                return stream.ToArray();
+            }
+        }
+    }
+}
